8d46c532b49fa71bc2f268baf151554f048fb6a7
Ridwaan Mohamed Isshack Jawaheer <ridwaan.m.jawaheer@accenture.com>
2020-02-20 15:40:06 +0400

    Initial skeleton

--- main/main.ino
+++ main/main.ino
@@ -2,35 +2,59 @@
 #include <Zumo32U4.h>
 
 
-Zumo32U4ButtonA buttonA;
-Zumo32U4ButtonB buttonB;
-Zumo32U4ButtonC buttonC;
+Zumo32U4ButtonA btnA;
+Zumo32U4ButtonB btnB;
+Zumo32U4ButtonC btnC;
 Zumo32U4LCD lcd;
 
-void setup() {
-  // put your setup code here, to run once:
+// Calculate battery life
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  result = ADCL;
+  result |= ADCH<<8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return (60000 - result) / 1000 * 100;
+}
 
-  lcd.clear();
-  lcd.print(F("Press A for challenge 1, Press B for challenge 2, Press C for challenge 3"));
-  buttonA.waitForButton();
-  buttonB.waitForButton();
-  buttonC.waitForButton();
+void startChallengeOne() {
+  // Draw Accenture logo
+  
+}
+
+void startChallengeTwo() {
+  // Follow path
+
+}
+
+void startChallengeThree() {
+  // Complete race track
+
+}
+
+void setup() {
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+
   lcd.clear();
+  lcd.println( readVcc(), DEC );
+  delay(1000);
 
-  if (buttonA.isPressed()) {
-    lcd.print(F("Challenge 1..."));
+  if(btnA.isPressed()) {
+    startChallengeOne();
   }
 
-  if (buttonB.isPressed()) {
-    lcd.print(F("Challenge 2..."));
+  if(btnB.isPressed()) {
+    startChallengeTwo();
   }
 
-  if (buttonC.isPressed()) {
-    lcd.print(F("Challenge 3..."));
+  if(btnC.isPressed()) {
+    startChallengeThree();
   }
 }
